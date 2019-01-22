7d86485e873a284ad24a1e45906d2b181516d934
Min <barrs.mindy@gmail.com>
2019-01-22 13:58:35 -0500

    Added Direction Options

--- Background-Generater-App/script.js
+++ Background-Generater-App/script.js
@@ -1,21 +1,44 @@
 /* VARIABLES */
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+let direction = document.getElementsByName("directionSet");
+let css = document.getElementById("code");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
  
 /* FUNCTIONS */
+function getSelectedRadio(){
+    var i;
+    for(i = 0; i < direction.length; i++){
+        if(direction[i].checked === true)
+            return direction[i].value;
+    }
+}
+
 function setGradient(){
-    body.style.background = 
-        "linear-gradient(to right, " 
-        + color1.value 
-        + ", " 
-        + color2.value 
-        + ")";
+    let selectedDirection = getSelectedRadio();
+    let css_color;
+
+    if(selectedDirection === 'radial-gradient'){
+        css_color =  selectedDirection + "("
+                        + color1.value 
+                        + ", " 
+                        + color2.value 
+                        + ")";
+    }else{
+        css_color = "linear-gradient(" + selectedDirection + ", " 
+                        + color1.value 
+                        + ", " 
+                        + color2.value 
+                        + ")";
+    }
+
+    body.style.background = css_color;
+    
 
     css.textContent = body.style.background + ";";
 }
 
-/* EVENTS */
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+/* EVENTS 
+color1.addEventListener('.color1', setGradient);
+color2.addEventListener('input[type="color"]', setGradient);*/
